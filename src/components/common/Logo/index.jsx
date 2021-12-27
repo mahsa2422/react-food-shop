8aa97a0ee3da1ded83f84af3db592ba4b097a1db
mahsa2422 <75795815+mahsa2422@users.noreply.github.com>
2021-12-27 11:24:11 +0330

    first commit

---
+++ src/components/common/Logo/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.png';
+import './styles.css';
+
+const Logo = () => (
+  <div className='header-content_logo'>
+    <div>
+    
+    <i class="fas fa-utensils"></i>
+      <span>
+        <b>Taste</b>
+      </span>
+    </div>
+    <p>
+      <b>Restaurant & BBQ</b>
+    </p>
+  </div>
+);
+
+export default Logo;
